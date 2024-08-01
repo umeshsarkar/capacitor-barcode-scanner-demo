f367f3f677da81e16609d90cbb23bfaa09d0466b
Umesh Sevra <sevra@atb-bremen.de>
2024-08-01 17:39:40 +0200

    android app and the demo added

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-cap-barcode-scanner',
+  webDir: 'www'
+};
+
+export default config;
